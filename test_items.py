f8f4f33bb2ecacb2b2631370f77b0784dad918e1
maksim-aleks01 <maksim_aleks01@mail.ru>
2022-05-26 20:44:06 +0300

    Add files via upload
    Реализована возможность выбора языка передаваемого в качестве параметра через командную строку.
    Реализована возможность выбора браузера передаваемого в качестве параметра через командную строку, если параметр не указан в качестве дефолтного значения будет использован браузер Google Chrome.

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time #Для проверки
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_cart_button(browser):
+    browser.get(link)
+    time.sleep(30) #Для визуальной оценки результатов теста в барузере
+
+    actual_result = browser.find_elements_by_css_selector("#asdd_to_basket_form > button") 
+    assert str(actual_result) != "[]", "Cart button is missing"
