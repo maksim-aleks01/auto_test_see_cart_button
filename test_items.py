372ee8c7d3344f0e510950392d7d6f2f44e76c0f
maksim-aleks01 <maksim_aleks01@mail.ru>
2022-05-27 22:26:18 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ def test_guest_should_see_cart_button(browser):
     browser.get(link)
     time.sleep(30) #Для визуальной оценки результатов теста в барузере
 
-    actual_result = browser.find_elements_by_css_selector("#asdd_to_basket_form > button") 
+    actual_result = browser.find_elements_by_css_selector("#add_to_basket_form > button") 
     assert str(actual_result) != "[]", "Cart button is missing"
